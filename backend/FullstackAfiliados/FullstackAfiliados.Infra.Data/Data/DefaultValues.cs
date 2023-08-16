05a1f0d6b3f666c2f73ab4f5c0c7a6a7c2e03904
Gregório <arthurgregorioleal@gmail.com>
2023-08-16 20:33:54 -0300

    Fix: #6 removido código inutilizado

--- backend/FullstackAfiliados/FullstackAfiliados.Infra.Data/Data/DefaultValues.cs
+++ backend/FullstackAfiliados/FullstackAfiliados.Infra.Data/Data/DefaultValues.cs
@@ -3,6 +3,5 @@ namespace FullstackAfiliados.Infra.Data.Data
     public static class DefaultValues
     {
         public const int VarcharLenght = 2000;
-        public const int FederalRegistrationLenght = 14;
     }
 }
