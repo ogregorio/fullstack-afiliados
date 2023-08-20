ba80cf514fe6c3aa1417a1f9aaf77e449266e3aa
Gregório <arthurgregorioleal@gmail.com>
2023-08-20 19:12:25 -0300

    Feat: #1 criado controller de recebimento de transações em arquivos

---
+++ backend/FullstackAfiliados/FullstackAfiliados.Api/Controllers/TransactionsController.cs
@@ -0,0 +1,29 @@
+using FullstackAfiliados.Api.Controllers.Base;
+using FullstackAfiliados.Application.UseCases.Transactions.Request;
+using FullstackAfiliados.Application.UseCases.Transactions.Response;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FullstackAfiliados.Api.Controllers;
+
+public class TransactionsController: MainController
+{
+    private readonly IMediator _mediator;
+
+    public TransactionsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    /// <summary>
+    /// Validate template
+    /// </summary>
+    /// <returns></returns>
+    [Consumes("multipart/form-data")]
+    [HttpPost("file")]
+    public async Task<ActionResult<TransactionsFromFileResponse>> PostAsync([FromForm] IFormFile file)
+    {
+        var result = await _mediator.Send(new TransactionsFromFileRequest { File = file });
+        return Ok(result);
+    }
+}
