25ba623d18854d3b9871c2139531733558dc2ab9
Gregório <arthurgregorioleal@gmail.com>
2023-08-20 20:55:37 -0300

    Fix: #3 melhoras nas menssagens de erro da leitura do arquivo

--- backend/FullstackAfiliados/FullstackAfiliados.Infra.CrosCutting.Commons/Helpers/SalesFile.cs
+++ backend/FullstackAfiliados/FullstackAfiliados.Infra.CrosCutting.Commons/Helpers/SalesFile.cs
@@ -1,28 +1,38 @@
 using System.Globalization;
 using FullstackAfiliados.Domain.Entities;
+using FullstackAfiliados.Infra.CrosCutting.Exceptions;
 
 namespace FullstackAfiliados.Infra.CrosCutting.Helpers;
 
 public class SalesFile
 {
+    const string DATE_FORMAT = "yyyy-MM-ddTHH:mm:ssK";
+
     public List<Transaction> Parse(string content)
     {
         List<Transaction> transactions = new List<Transaction>();
         string[] lines = content.Split('\n');
 
-        foreach (string line in lines)
+        try
         {
-            if (!String.IsNullOrWhiteSpace(line))
+            foreach (string line in lines)
             {
-                transactions.Add(new Transaction
+                if (!String.IsNullOrWhiteSpace(line))
                 {
-                    RelativeType = int.Parse(line.Substring(0, 1)),
-                    Date = DateTime.ParseExact(line.Substring(1, 25), "yyyy-MM-ddTHH:mm:ssK", CultureInfo.InvariantCulture),
-                    Product = line.Substring(26, 30).Trim(),
-                    Amount = decimal.Parse(line.Substring(56, 10)) / 100,
-                    Salesman = line.Substring(66).Trim()
-                });
+                    transactions.Add(new Transaction
+                    {
+                        RelativeType = int.Parse(line.Substring(0, 1)),
+                        Date = DateTime.ParseExact(line.Substring(1, 25), DATE_FORMAT, CultureInfo.InvariantCulture),
+                        Product = line.Substring(26, 30).Trim(),
+                        Amount = decimal.Parse(line.Substring(56, 10)) / 100,
+                        Salesman = line.Substring(66).Trim()
+                    });
+                }
             }
+        }
+        catch (Exception e)
+        {
+            throw new BadRequestException("Invalid line in transactions file");
         }
 
         return transactions;
