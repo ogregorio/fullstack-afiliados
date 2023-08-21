1bd3fb9db57e01bbf9a5081a3de254716c52fa55
Gregório <arthurgregorioleal@gmail.com>
2023-08-21 20:59:28 -0300

    Feat: #1 adicionado header e footer na aplicação

--- frontend/FullstackAfiliados/src/App.tsx
+++ frontend/FullstackAfiliados/src/App.tsx
@@ -4,16 +4,24 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import './langs/i18n';
 import Login from '@pages/login';
+import Header from '@cp/molecules/header';
+import Footer from '@cp/molecules/footer';
 
 export default function App() {
   return (
     <LocalizationProvider dateAdapter={AdapterMoment}>
       <ThemeProvider>
+        <header>
+          <Header />
+        </header>
         <Router>
           <Routes>
             <Route path='/login' element={<Login />} />
           </Routes>
         </Router>
+        <footer>
+          <Footer />
+        </footer>
       </ThemeProvider>
     </LocalizationProvider>
   );
