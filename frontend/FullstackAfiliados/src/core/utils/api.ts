3f81e025baec5028701df8f07b92989d064b2cff
Gregório <arthurgregorioleal@gmail.com>
2023-08-23 19:15:21 -0300

    Fix: #8 corrigido bug ao definir o token no cabeçalho da requisição do frontend

--- frontend/FullstackAfiliados/src/core/utils/api.ts
+++ frontend/FullstackAfiliados/src/core/utils/api.ts
@@ -1,19 +1,39 @@
 import axios from 'axios';
 
-const getToken = (): string | null => {
-  const local = localStorage.getItem('user');
-  return local ? JSON.parse(local).token : null;
-};
-
-const token = getToken();
-
 const apiInstance = axios.create({
   baseURL: 'http://localhost:5001/',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
-    ...(token && { 'Authorization': 'Bearer ' + token }),
   },
 });
+
+const getToken = (): string | null => {
+  const local = localStorage.getItem('user');
+  return local ? JSON.parse(local).token : null;
+};
+
+const setAuthorizationHeader = (token: string | null) => {
+  if (token) {
+    apiInstance.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+  } else {
+    delete apiInstance.defaults.headers.common['Authorization'];
+  }
+};
+
+const configureInterceptor = () => {
+  const token = getToken();
+  setAuthorizationHeader(token);
+
+  apiInstance.interceptors.request.use((config) => {
+    const updatedToken = getToken();
+    if (updatedToken) {
+      config.headers['Authorization'] = 'Bearer ' + updatedToken;
+    }
+    return config;
+  });
+};
+
+configureInterceptor();
 
 export default apiInstance;
